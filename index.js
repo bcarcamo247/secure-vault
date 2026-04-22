2f8f8c58fb6c6aea0aa24aec2d925c61fac5d596
bcarcamo247 <bcarcamo247@gmail.com>
2026-04-22 17:23:29 -0400

    Finished secure vault

---
+++ index.js
@@ -0,0 +1,19 @@
+let code1;
+
+code1 = 7 + 3;
+
+let code2 = (400 - 200) / 5;
+
+let code3 = 38 + (1 % 7);
+
+let message = "The vault has been secured. The combination is: ";
+
+let codeA = code1 + "-" + code2 + "-" + code3;
+
+let codeB = `${code1}-${code2}-${code3}`;
+
+console.log(message);
+
+console.log(codeA);
+
+console.log(codeB);
